3da0cc2fe91587b55bf468fbe9efab9381d0ecc9
Ujjwal <utamrakar3@gmail.com>
2025-01-28 12:47:24 +0530

    Update app title and added Roadmaps

---
+++ src/app/roadmaps/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Roadmaps() {
+  return (
+    <>
+    <h1 className='text-4xl font-bold text-center'>
+        Roadmaps
+    </h1>
+    </>
+  )
+}
+
