177ab49ffdaf5ddace9d559f2ecafdd2d1b5daf0
Pranav Pawar <pranavpawar1379@gmail..com>
2025-02-01 00:44:02 +0530

    created new pages and added a basic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import Link from 'next/link'
 function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <>
+      <div className='p-4 border-b-4 flex justify-between static'>
+        <div>
+        <Link href="/" className='font-bold font-mono text-l text-gray-600'>LearnTech </Link>
+
+        </div>
+        <div >
+
+        <Link className='px-6' href="/posts"> Posts</Link>
+        <Link className='px-6' href="/roadmaps"> Roadmaps</Link>
+        <Link className='px-6' href="/profile">Profile </Link>
+        </div>
+      </div>
+    </>
   )
 }
 
