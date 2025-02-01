1f3c0383faa8903315c9108e8da51219e040ed05
Ujjwal <utamrakar3@gmail.com>
2025-02-01 14:29:08 +0530

    Add roadmap data and implement dynamic rendering for roadmap pages

--- src/app/roadmaps/[slug]/page.tsx
+++ src/app/roadmaps/[slug]/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import roadmapData from '@/data/data.json';
 
-function Slug() {
-  return (
-    <div>Roadmap</div>
-  )
+interface PageProps {
+  params: {
+    slug: string;
+  }
 }
 
-export default Slug
+interface Roadmap{
+  [key: string]: {}
+}
+
+export default function RoadmapPage({ params }: PageProps) {
+  const { slug } = params;
+  const roadmaps : Roadmap = roadmapData;
+
+  if (!roadmaps[slug]) {
+    return <div className='text-red-500'>Roadmap not found</div>;
+  }
+
+  const ArrowDown = () => (
+    <div className="flex justify-center my-4">
+      <svg width="40" height="40" viewBox="0 0 24 24" className="text-gray-400 animate-bounce">
+        <path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/>
+      </svg>
+    </div>
+  );
+
+  return (
+    <div>
+      <h1>Current Roadmap: {slug}</h1>
+      <ArrowDown />
+    </div>
+  )
+}
